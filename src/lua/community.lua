d4d5bf1b4e28d020a5a6e962608381ab838908f1
smashell <aliwong1980@gmail.com>
2025-07-22 18:49:39 +0800

    Update community.lua

--- src/lua/community.lua
+++ src/lua/community.lua
@@ -32,7 +32,8 @@ return {
   { import = "astrocommunity.motion.hop-nvim" },
   { import = "astrocommunity.motion.nvim-surround" },
   { import = "astrocommunity.test.neotest" },
-  { import = "astrocommunity.markdown-and-latex.markdown-preview-nvim" },
+  -- { import = "astrocommunity.markdown-and-latex.markdown-preview-nvim" },
+  { import = "astrocommunity.markdown-and-latex.markview-nvim" },
 
   { import = "astrocommunity.colorscheme.nightfox-nvim" },
   { import = "astrocommunity.colorscheme.neofusion-nvim" },
